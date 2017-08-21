d831775e26ac3d75a7210e520fd2a7074126a738
R-prakash25 <31201464+R-prakash25@users.noreply.github.com>
2017-08-21 19:28:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var element=document.getElementById('main.txt');
+element.innerHTML='New value';
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft + 'px';
+}
+img.Onclick=function(){
+    var interval=setInterval(moveright,100);
+};
